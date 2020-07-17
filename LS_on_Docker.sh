c569ec136505f6f4a6f48c40b26ed6c6e908b856
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-07-17 15:36:21 -0500

    Add files via upload

---
+++ LS_on_Docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Author: Piyush Kumar (piyushkumar.jiit@gmail.com)
+#Step 1: Download; Step 2: Grant permission to script by "chmod 755 LS_on_Docker.sh" ; Step 3: ./LS_on_Docker.sh | tee LS_Install.log
